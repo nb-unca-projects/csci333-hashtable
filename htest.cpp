7fd79b39fca6e847bb47cf1758859ea474d74410
Nick Bury <nick@debian.(none)>
2012-12-03 21:07:49 -0500

    everything appears to be working

--- htest.cpp
+++ htest.cpp
@@ -5,17 +5,17 @@
 using namespace std;
 
 int main() {
-  Hash<int>* h = new Hash<int>(0);
+  Hash<string>* h = new Hash<string>("");
   
-  h->insert("a", 2);
-  h->insert("b", 8);
-  h->insert("c", 7);
-  h->insert("d", 6);
-  h->insert("e", 5);
-  h->insert("a", 3);
+  h->insert("a", "2");
+  h->insert("b","8");
+  h->insert("c", "7");
+  h->insert("d", "6");
+  h->insert("e", "5");
+  h->insert("a", "3");
   
   h->remove("c");
-  cout << h->find("c") << endl;
+  cout << h->find("d") << endl;
 
   return 0;
 }
